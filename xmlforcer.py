771c6648aaf2bb98ef9632cc61cac6e14d6cd4c5
fexsecc <165517769+fexsecc@users.noreply.github.com>
2024-12-16 20:46:56 +0200

    initial commit

---
+++ xmlforcer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''
+https://fexsec.net
+https://github.com/fexsecc
+https://app.hackthebox.com/users/1842563
+'''
+
+import argparse
+import requests
+import threading
+from colorama import Fore, Style
+
+class Forcer:
